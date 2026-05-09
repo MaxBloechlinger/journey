c8071e6de1e6f0224f99b8423fe54e6ff179b280
MaxBloechlinger <max.bloechlinger.job@gmail.com>
2026-05-09 09:27:13 +0200

    feat(ui): add authModal state to uiStore

--- src/store/uiStore.ts
+++ src/store/uiStore.ts
@@ -2,20 +2,26 @@ import { create } from 'zustand'
 
 interface UIStore {
   aiSidebarOpen: boolean
+  authModalOpen: boolean
   activeSegmentId: string | null
   editingItemId: string | null
 
   toggleAISidebar: () => void
+  openAuthModal: () => void
+  closeAuthModal: () => void
   setActiveSegment: (id: string | null) => void
   setEditingItem: (id: string | null) => void
 }
 
 export const useUIStore = create<UIStore>()((set) => ({
   aiSidebarOpen: false,
+  authModalOpen: false,
   activeSegmentId: null,
   editingItemId: null,
 
   toggleAISidebar: () => set((s) => ({ aiSidebarOpen: !s.aiSidebarOpen })),
+  openAuthModal: () => set({ authModalOpen: true }),
+  closeAuthModal: () => set({ authModalOpen: false }),
   setActiveSegment: (id) => set({ activeSegmentId: id }),
   setEditingItem: (id) => set({ editingItemId: id }),
 }))
