10708cd6f5649e194bb1d5073a5a9e406885a123
MaxBloechlinger <max.bloechlinger.job@gmail.com>
2026-05-10 09:42:32 +0200

    feat(ui): add mobileMapOpen state and toggleMobileMap to uiStore

--- src/store/uiStore.ts
+++ src/store/uiStore.ts
@@ -3,12 +3,14 @@ import { create } from 'zustand'
 interface UIStore {
   aiSidebarOpen: boolean
   authModalOpen: boolean
+  mobileMapOpen: boolean
   activeSegmentId: string | null
   editingItemId: string | null
 
   toggleAISidebar: () => void
   openAuthModal: () => void
   closeAuthModal: () => void
+  toggleMobileMap: () => void
   setActiveSegment: (id: string | null) => void
   setEditingItem: (id: string | null) => void
 }
@@ -16,12 +18,14 @@ interface UIStore {
 export const useUIStore = create<UIStore>()((set) => ({
   aiSidebarOpen: false,
   authModalOpen: false,
+  mobileMapOpen: false,
   activeSegmentId: null,
   editingItemId: null,
 
   toggleAISidebar: () => set((s) => ({ aiSidebarOpen: !s.aiSidebarOpen })),
   openAuthModal: () => set({ authModalOpen: true }),
   closeAuthModal: () => set({ authModalOpen: false }),
+  toggleMobileMap: () => set((s) => ({ mobileMapOpen: !s.mobileMapOpen })),
   setActiveSegment: (id) => set({ activeSegmentId: id }),
   setEditingItem: (id) => set({ editingItemId: id }),
 }))
